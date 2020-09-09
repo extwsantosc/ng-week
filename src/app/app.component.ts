de54cf7a610e261fbfd4477698f94aa552bf8501
Carlos Tangarife <carlos.tangarife@ceiba.com.co>
2020-09-09 14:29:24 -0500

    se cambia la referencia a la subscripción de la petición y se cambia por el asynPipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,19 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CharactersService } from './service/characters.service';
 import { Character } from './model/character.model';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  constructor(private readonly charactersService: CharactersService) { }
+export class AppComponent implements OnInit {
+  constructor(protected readonly charactersService: CharactersService) { }
 
-  public isLoading: boolean;
-  public characters: Character[];
-
-  private subscription: Subscription;
+  public characters$: Observable<Character[]>;
 
   ngOnInit(): void {
-    this.isLoading = true;
-    this.subscription = this.charactersService.getAll().subscribe({
-      next: characters => {
-        this.characters = characters;
-      },
-      complete: () => {
-        this.isLoading = false;
-      },
-      error: (err) => {
-        console.error('An error occurred', err);
-        
-      }
-    });
+    this.characters$ = this.charactersService.getAll();
   }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-
 }
